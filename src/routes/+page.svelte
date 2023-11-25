96e782703856eae77bfe36ad1f24effa9d223e13
Kube Kubow <kubow@users.noreply.github.com>
2023-11-25 11:03:43 +0100

    first not fully working example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,58 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { setContext } from "https://demos.cloud.gooddata.com/components/ecommerce-parent-dev.js";
+  import factory, {
+    TigerTokenAuthProvider,
+  } from "https://demos.cloud.gooddata.com/components/tigerBackend.js";
+
+  setContext({
+    backend: factory()
+      .onHostname("https://demos.cloud.gooddata.com")
+      .withAuthentication(
+        new TigerTokenAuthProvider(
+          "amFrdWIudmFqZGE6dnVlOmlXdGhEUmJqSnpHdEVORllCci92dWtySEllUnVXL3VF"
+        )
+      ),
+    workspaceId: "ecommerce-parent-dev",
+  });
+</script>
+
+<div id="content">
+  <div id="left">
+    <p>Left side with an Insight:</p>
+    <gd-insight
+      insight="8e8f236e-1bb2-48b3-828e-48706c684629"
+      title="top 10 performers"
+      style="height: 700px;"
+    >
+    </gd-insight>
+  </div>
+  <div id="right">
+    <p>Right side with the dashboard:</p>
+    <gd-dashboard dashboard="092929af-375a-4e9c-964f-2add8cdbd259">
+    </gd-dashboard>
+  </div>
+</div>
+
+<style>
+  #content {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    padding: 0;
+    margin: 0;
+  }
+
+  #left {
+    flex-direction: column;
+    margin: 0 auto 2rem;
+    padding: 0;
+    min-width: 400px;
+  }
+
+  #right {
+    flex-direction: column;
+    margin: 0 auto 2rem;
+    padding: 0;
+    min-width: 700px;
+  }
+</style>
